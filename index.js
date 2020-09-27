2da0a5add45b6255cf55bccb98dcdaf7fdaab3aa
Leonel Velez <leonelvelez314@gmail.com>
2020-09-27 16:40:05 -0500

    add the quicksort algoritm

--- index.js
+++ index.js
@@ -1,21 +1,81 @@
 
 
+const DES_ = 'DES';
+const ASC_ = 'ASC';
+
+
 class Order{
-    arrays(arraytoOrder, typeOrder = 'ASC')
+    
+    static arrays(arraytoOrder, typeOrder = 'ASC')
     {
-        try{            
+        let arrayToReturn = [];
+        try
+        {            
           switch(typeOrder)      
           {            
+            case 'ASC':
+                arrayToReturn = quickSort(arraytoOrder);
+                break;
+
+            case 'DES':
+                arrayToReturn = quickSort(arraytoOrder);
+                break;
+            default:
+                arrayToReturn = arraytoOrder;
+                break;
           }
-          for(let iterator = 0; iterator <= arraytoOrder.length; iterator++)      
-          {
-            
-          }        
+          
+        return arrayToReturn;
         }catch(warnMessage)
         {
             console.warn(warnMessage);
         }
+    }    
+}
+
+
+
+quickSort = (arra) =>{
+
+    if(arra.length > 1)
+    {
+        let returnOrder = orderSort(arra);        
+        
+        return quickSort(returnOrder.ArrayLeft).concat(returnOrder.Pivote, quickSort(returnOrder.ArrayRigth))
+    }else{
+        return arra;
     }
 }
 
-module.exports = Order;
+
+
+orderSort = (arra)=>
+{
+    const pivote  = arra[0];
+    let arrayleft  = [];
+    let arrayrigth  = [];
+    for(let iterator = 1; iterator < arra.length;  iterator++)
+    {
+        if(arra[iterator] < pivote)
+        {
+            arrayleft.push(arra[iterator])
+        }else{
+            arrayrigth.push(arra[iterator])
+        }
+    }
+        
+    pivoteArr = [pivote]
+    objectReturn = {
+        ArrayLeft : arrayleft,
+        ArrayRigth : arrayrigth,
+        Pivote : pivoteArr
+    }
+    return objectReturn;
+}
+
+
+
+
+console.log(Order.arrays([1,4,2,8,5], ASC_))
+
+module.exports = {Order, DES_, ASC_};
