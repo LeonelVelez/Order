a5b49619d47159f2fb84e820b9150404f0bd332b
Leonel Velez <leonelvelez314@gmail.com>
2020-09-26 22:45:07 -0500

    add an js index file

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+class Order{
+    arrays(arraytoOrder, typeOrder = 'ASC')
+    {
+        try{            
+          switch(typeOrder)      
+          {            
+          }
+          for(let iterator = 0; iterator <= arraytoOrder.length; iterator++)      
+          {
+            
+          }        
+        }catch(warnMessage)
+        {
+            console.warn(warnMessage);
+        }
+    }
+}
+
+module.exports = Order;
